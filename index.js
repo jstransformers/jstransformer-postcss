df27891c2d33e79ce27f7cfecd2aca5b54100d3d
tunnckoCore <mameto_100@mail.bg>
2015-08-05 22:38:12 +0300

    100% coverage

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ exports.outputFormat = 'css';
 
 exports.render = function (str, opts) {
   opts = opts && typeof opts === 'object' ? opts : {};
-  opts.plugins = opts.plugins && opts.plugins.length ? arrayify(opts.plugins) : []
+  opts.plugins = opts.plugins && opts.plugins.length
+    ? (!Array.isArray(opts.plugins) ? [opts.plugins] : opts.plugins)
+    : []
 
   return postcss(opts.plugins).process(str, opts).css
 };
-
-function arrayify (val) {
-  return !Array.isArray(val) ? [val] : val;
-}

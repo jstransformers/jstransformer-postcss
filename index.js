318479129c56613f6e0174b8b86eb0c08d9d5869
Rob Loach <robloach@gmail.com>
2015-09-09 10:03:18 -0400

    Update postcss to ^5.0.4

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 'use strict';
 
+var Promise = require('promise');
 var postcss = require('postcss');
 
 exports.name = 'postcss';
 exports.inputFormats = ['postcss', 'css'];
 exports.outputFormat = 'css';
 
-exports.render = function (str, opts) {
-  opts = opts && typeof opts === 'object' ? opts : {};
-  opts.plugins = opts.plugins && opts.plugins.length
-    ? (!Array.isArray(opts.plugins) ? [opts.plugins] : opts.plugins)
-    : []
-
-  return postcss(opts.plugins).process(str, opts).css
+exports.renderAsync = function (str, options) {
+  return new Promise(function (resolve, reject) {
+    var plugins = []
+    // TODO: Support having an array of plugin objects, or strings.
+    var pluginsToLoad = options.plugins || []
+    for (var i in pluginsToLoad) {
+      var plugin = null
+      switch (typeof i) {
+        case 'string':
+          plugin = require(i)(pluginsToLoad[i])
+          break
+        case 'object':
+        default:
+          plugin = i
+          break
+      }
+      if (plugin) {
+        plugins.push(plugin)
+      }
+    }
+    postcss(plugins).process(str, options).then(function (result) {
+      // TODO: Add result.map to "dependencies".
+      resolve(result.css)
+    }, reject)
+  })
 };
